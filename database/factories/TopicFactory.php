f627a3b257f7b712c9cf0ea72af583ba397fb161
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-12-18 10:52:05 +0200

    created Post model,migration,factory created Topic model,migration,factory improved DatabaseSeeder

---
+++ database/factories/TopicFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Topic;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TopicFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Topic::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        $name = $this->faker->words('2',true);
+
+        return [
+            'slug'=> Str::of($name)->slug('-')->__toString(),
+            'name'=>$name
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+        $topics = Topic::factory()->count(5)->create();
+        $users = User::factory(12)->create();
+
+        $users->each(function ($user) use($topics) {
+
+            $topics->each(function($topic) use ($user){
+                Post::factory()->count(2)->create([
+                    'author_id'=>$user->id,
+                    'topic_id'=>$topic->id,
+                ]);
+            });
+
+        });
     }
 }
