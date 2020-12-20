69ac1149c277acb386a504cfd266976d60eb42a7
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-12-20 21:51:34 +0200

    created Cors middleware and added it to base middlewares created first Apollo query for PostList

--- resources/js/views/PostList.vue
+++ resources/js/views/PostList.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        Post List
+       <ul>
+           <li v-for="(post,index) in posts.data" v-text="post.title" :key="post.id"></li>
+       </ul>
     </div>
 </template>
 
 <script>
+    import gql from 'graphql-tag'
+
     export default {
-        name: "PostList"
+        name: "PostList",
+        data(){
+            return {
+                posts:{}
+            }
+        },
+        apollo: {
+            posts: gql`{
+                posts {
+                    paginatorInfo{
+                        total
+                        perPage
+                        currentPage
+                        lastPage
+                    }
+                    data{
+                        id
+                        title
+                        lead
+                    }
+                 }
+            }`
+        }
     }
 </script>
 
