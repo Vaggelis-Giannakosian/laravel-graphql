c2705e49700fdb481d7fba69d92dd675e42e5972
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-12-24 12:24:30 +0200

    minor change to 404 div position

--- resources/js/views/NotFoundPage.vue
+++ resources/js/views/NotFoundPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-screen w-screen bg-gray-200 flex justify-center">
-        <div class="text-center text-gray-600 self-center">
+        <div class="text-center text-gray-600 self-center mb-20">
             <h1 class="text-6xl font-sans text-shadow">404</h1>
             <h3 class="text-4xl font-sans text-shadow">This page cannot be found</h3>
         </div>
